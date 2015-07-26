68f113b66de96b583bc84b43525e1b20426f6549
Sidhanth Mohanty <sidhanth@cmu.edu>
2015-07-26 23:20:13 +0530

    did some cleaning. ready to start implementing an ML algo

--- NeuralNetworks/neural.py
+++ NeuralNetworks/neural.py
@@ -1,19 +1,17 @@
 import cv2
 import numpy as np
 
-img_in = cv2.imread('images/in1.png', 0)
-img_out = cv2.imread('images/out1.png', 0)
+train_image = cv2.imread('images/in1.png', 0)
 #crop the image for consistency
 h1 = 0
 h2 = 300
 l1 = 0
 l2 = 700
 H1 = 0
-H2 = 300
 L1 = 0
-L2 = 700
-img_in = img_in[h1:h2, l1:l2]
-img_out = img_out[H1:H2, L1:L2]
+H2, L2 = train_image.shape[:2]
+img_in = train_image[h1:h2, l1:l2]
+img_out = train_image
 #make it into a long array
 start = np.reshape(img_in, (1, (l2-l1)*(h2-h1)))
 end = np.reshape(img_out, (1, (L2-L1)*(H2-H1)))
@@ -24,20 +22,21 @@ dimensions = [start.shape[1], 100, end.shape[1]]
 transitions = [np.random.randn(dimensions[x], dimensions[x+1]) for x in range(0, len(dimensions)-1)]
 print("Initialized transitions")
 def get_output():
-	S = start
-	for x in range(0, len(transitions)):
-		S = np.dot(S, transitions[x])
-		S = np.arctanh(S)
-	return S
+    S = start
+    for x in range(0, len(transitions)):
+        S = np.dot(S, transitions[x])
+        print S
+        S = np.tanh(S)
+    return S
 
 out = get_output()
 
-out = np.reshape(out, (h2-h1, l2-l1))
+out = np.reshape(out, (H2-H1, L2-L1))
 print(out)
 #cv2.imshow('image', img_in)
 #cv2.imshow('image2', img_out)
-cv2.imshow('output', out)
-cv2.waitKey(0)
+#cv2.imshow('output', out)
+#cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 # cv2.imwrite('out.png', img_in)
